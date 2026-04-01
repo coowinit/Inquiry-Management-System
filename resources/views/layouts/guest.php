27f09f53b78fd8c3d93092f1a4fc7e834ebde70d
winson <18604421@qq.com>
2026-04-01 17:17:33 +0800

    修改css文件的时间戳

--- resources/views/layouts/guest.php
+++ resources/views/layouts/guest.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= e(($pageTitle ?? 'System') . ' - ' . config('app.name')) ?></title>
-    <link rel="stylesheet" href="<?= e(asset('css/app.css')) ?>">
+    <link rel="stylesheet" href="<?= e(asset('css/app.css?v=1.0.1')) ?>">
 </head>
 <body class="guest-body">
     <?= $content ?>
